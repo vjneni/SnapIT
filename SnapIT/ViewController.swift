3dc4e1201379f18e7dccb5a977b760a4a3acc8e7
Vijay Nannapaneni <vijay.nannapaneni@sprint.com>
2017-04-17 11:21:59 -0500

    Initial Commit

---
+++ SnapIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapIT
+//
+//  Created by Nannapaneni, Vijay X [Levi, Ray & Shoup Inc Contractor for Sprint] on 4/17/17.
+//  Copyright © 2017 Crap App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
